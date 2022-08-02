e3928bec460c548884d272f72c2666ba53c000ad
nicoarkano <nicoarkano@gmail.com>
2022-08-02 13:00:19 -0300

    create crud

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,9 @@
-import Formularios from './components/Formularios.jsx'
-import Listas from './components/Listas.jsx'
-import Parrafo from './components/Parrafo.jsx'
-import Variables from './components/Variables.jsx'
-import Eventos from './components/Eventos.jsx'
-import Contador from './components/Contador.jsx'
 export function App() {
 
   return (
 	  <>
 	  <h1>Hello World! Preact</h1>
-	   <Parrafo />
-	   <hr/>
-		  <Variables />
-	   <hr/>
-		  <Eventos />
-	   <hr/>
-	   <Listas /> 
-	   <hr/>
-	  <Contador />
-	   <hr/>
-	  <Formularios />
+	  <p> Aqui escribo mi historia </p>
 	  </>
   )
 }
