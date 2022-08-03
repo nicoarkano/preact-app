550e3fe2ec2a1f939f519bc7f903ab38be921851
nicoarkano <nicoarkano@gmail.com>
2022-08-03 13:56:11 -0300

    Add shortid

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,26 @@
 import { useState } from "preact/hooks";
+import shortid from 'shortid'
 export function App() {
   const [tarea, setTarea] = useState("");
   const [tareas, setTareas] = useState([]);
   const agregarTarea = (e) => {
     e.preventDefault();
-	if (!tarea.trim()){ console.log("Este elemento esta vacio") 
-	return}
-	
+    if (!tarea.trim()) {
+      console.log("Este elemento esta vacio");
+      return;
+    }
 
-	
-	
-	setTareas([
-		...tareas,
-		{id: tareas.length, nombreTarea: tarea}
+    setTareas([...tareas, { id: shortid.generate(), nombreTarea: tarea }]);
 
-	])
+    setTarea("");
+  };
+  console.log(tareas);
 
-	
-
-	setTarea('')}
-	console.log(tareas)
-
-	const eliminarElemento = (id) => {
-		console.log(id)
-		const arrayFiltrado = tareas.filter((filterTask)=> filterTask.id !== id)
-		setTareas(arrayFiltrado)
-	}
+  const eliminarElemento = (id) => {
+    console.log(id);
+    const arrayFiltrado = tareas.filter((filterTask) => filterTask.id !== id);
+    setTareas(arrayFiltrado);
+  };
 
   return (
     <>
@@ -36,16 +31,20 @@ export function App() {
           <div class="col-8">
             <h4 class="text-center">Lista de Tareas</h4>
             <ul class="list-group">
-            {tareas.map( (task) =>   <li class="list-group-item" key ={task.id}>
-                <span className="lead">{task.nombreTarea}</span>
-                <button 
-				class="btn btn-danger btn-sm float-end mx-2"
-				onClick={() => eliminarElemento(task.id)}
-				>
-                  Eliminar
-                </button>
-                <button class="btn btn-warning btn-sm float-end">Editar</button>
-              </li>)}
+              {tareas.map((task) => (
+                <li class="list-group-item" key={task.id}>
+                  <span className="lead">{task.nombreTarea}</span>
+                  <button
+                    class="btn btn-danger btn-sm float-end mx-2"
+                    onClick={() => eliminarElemento(task.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button class="btn btn-warning btn-sm float-end">
+                    Editar
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="col-4">
@@ -56,12 +55,9 @@ export function App() {
                 className="form-control mb-2"
                 placeholder="ingrese tarea"
                 onchange={(e) => setTarea(e.target.value)}
-				value={tarea}
+                value={tarea}
               />
-              <button
-                className="btn btn-dark btn-sm col-12"
-                type="submit"
-              >
+              <button className="btn btn-dark btn-sm col-12" type="submit">
                 Agregar
               </button>
             </form>
